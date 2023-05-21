6ba27aedf78ec00244fbc18bcc9671c9cd1ed5db
Andrés Felipe Sanguino Sanguino <114168015+Summerhp@users.noreply.github.com>
2023-05-20 21:28:42 -0500

    Listado de ideas parcial

--- src/components/lider/Lider_Template.jsx
+++ src/components/lider/Lider_Template.jsx
@@ -8,7 +8,7 @@ export default function TemplateLider({ children }) {
         <div className="col-12 col-sm-2 m-0 p-0">
             <NavbarLU></NavbarLU>
         </div>
-        <div className="col-12 col-sm-10 m-0 p-0">
+        <div className="col-12 col-sm-10 m-0 p-0 d-flex align-items-center">
             <Outlet></Outlet>
         </div>
     </div></>);
