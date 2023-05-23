991109715a5670c8c51cbc3a244f33da036c48d4
Andrés Felipe Sanguino Sanguino <114168015+Summerhp@users.noreply.github.com>
2023-05-22 23:20:43 -0500

    Se cargan datos desde axios de estudiantes y docentes  (#23)
    * Listado de ideas
    
    * Listado de ideas parcial
    
    * Mensaje de alert con fallas de persistencia
    
    * Correción de lista y vista de idea
    
    * Delete Tabla_idea.jsx
    
    * Correciones del merge
    
    * Cargue de lo combobox
    
    Se cargó los estudiantes y docentes desde un axios y se acomodó los filtross

--- src/components/lider/Listar_ideas.jsx
+++ src/components/lider/Listar_ideas.jsx
@@ -1,4 +1,6 @@
+import React, { useEffect, useState } from 'react'
 import Tabla from "./Lider_Tabla_idea";
+import axios from "axios";
 
 export default function Listar_Ideas() {
     return (
@@ -12,20 +14,20 @@ export default function Listar_Ideas() {
                         <Tabla></Tabla>
                         <br></br>
                         <div className="d-flex justify-content-end">
-                            <button type="button" class="btn rounded-3" style={{ background: "#1C3B57", color: "#FFFFFF" }}>
-                                <div className="row">
-                                    <div className="col-auto">
-                                        Generar informe
-                                    </div>
-                                    <div className="col-auto">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
-                                            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
-                                        </svg>
-                                    </div>
-                                </div>
-                            </button>
-                        </div>
+                        <button type="button" className="btn rounded-3" style={{background: "#1C3B57", color: "#FFFFFF"}}>
+                <div className="row">
+                    <div className="col-auto">
+                        Generar informe
+                    </div>
+                    <div className="col-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
+</svg>
+                    </div>
+                </div>                    
+              </button> 
+                        </div>                          
                     </div>
                 </div>
             </div>
@@ -33,47 +35,93 @@ export default function Listar_Ideas() {
     );
 }
 function Filtros() {
-    return (<form className="row g-7">
-        <div className="col-auto d-flex align-items-center mb-2">
-            <select className="form-select-sm selector fw-bold text-black">
+    return (<form className="row gy-2 gx-1">
+        <div className="col-auto d-flex align-items-center mb-1">
+            <select name="tutor" className="form-select-sm selector fw-bold text-black">
                 <option selected="0">Tutor</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+                <Getdocentes></Getdocentes>
             </select>
         </div>
-        <div className="col-auto d-flex align-items-center mb-2">
-            <select className="form-select-sm selector fw-bold text-black">
+        <div className="col-auto d-flex align-items-center mb-1">
+            <select name="estudiante" className="form-select-sm selector fw-bold text-black">
                 <option selected="0">Estudiante</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+                <Getestudiantes></Getestudiantes>
             </select>
         </div>
-        <div className="col-auto d-flex align-items-center mb-2">
-            <select className="form-select-sm selector fw-bold text-black">
-                <option selected="0">Area</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+        <div className="col-auto d-flex align-items-center mb-1">
+            <select name="actividad" className="form-select-sm selector fw-bold text-black">
+                <option selected="0">Actividad</option>
+                <option value="minera">Minera</option>
+                <option value="agrupecuaria">Agropecuaria</option>
+                <option value="comercial">Comercial</option>
+                <option value="servicios">Servicios</option>
+                <option value="industrial">Industrial</option>
             </select>
         </div>
-        <div className="col-auto d-flex align-items-center mb-2">
-            <select className="form-select-sm selector fw-bold text-black">
-                <option selected="0">Estado</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+        <div className="col-auto d-flex align-items-center mb-1">
+            <select name="estado" className="form-select-sm selector fw-bold text-black">       
+                <option selected="0">Estado</option>         
+                <option value="aprobada">Aprobada</option>
+                <option value="desaprobada">Desaprobada</option>
+                <option value="vencida">Vencida</option>
+                <option value="formulacion">Formulación</option>
+                <option value="formulacion">Pendiente</option>
             </select>
         </div>
-        <div className="col-auto d-flex align-items-center mb-2">
-            <input type="date" className="fw-bold text-black form-control-sm" id="start" required name="fecha_inicio" min="1800-01-01" max="2040-12-31"></input>
+        <div className="col-auto d-flex align-items-center mb-1">
+            <input name="fecha_inicio" type="date" className="fw-bold text-black form-control-sm" id="start"  min="1800-01-01" max="2040-12-31"></input>
         </div>
-        <div className="col-auto d-flex align-items-center mb-2">
-            <input type="date" className="fw-bold text-black form-control-sm" id="finish" required name="fecha_fin" min="1800-01-01" max="2040-12-31"></input>
+        <div className="col-auto d-flex align-items-center mb-1">
+            <input name="fecha_fin" type="date" className="fw-bold text-black form-control-sm" id="finish" min="1800-01-01" max="2040-12-31"></input>
         </div>
-        <div className="col-auto d-flex align-items-center mb-2">
-            <button type="button" className="btn btn-warning fw-bold text-black">Aplicar</button>
+        <div className="col-auto d-flex align-items-center mb-1">
+            <button type="submit" className="btn btn-warning fw-bold text-black">Aplicar</button>
         </div>
     </form>)
+}
+function Getdocentes () {
+    const [datos, setDatos] = useState([]);
+    const getDocentes = async () => {
+        let value = null;
+        value = await axios.get('../docentes.json').then(
+          response => {
+            const data = response.data;
+            return data;
+          }).catch(error => {
+            console.error(error);
+          });
+        setDatos(value)
+      };
+      useEffect(() => {
+        getDocentes();
+      }, []);
+      return(
+        datos.map((d) => {
+            return (
+                <option value={d.id}>{d.docente}</option>
+        )})
+      )       
+}
+function Getestudiantes () {
+    const [datos2, setDatos] = useState([]);
+    const getEstudiantes = async () => {
+        let value = null;
+        value = await axios.get('../estudiantes.json').then(
+          response => {
+            const data = response.data;
+            return data;
+          }).catch(error => {
+            console.error(error);
+          });
+        setDatos(value)
+      };
+      useEffect(() => {
+        getEstudiantes();
+      }, []);
+      return(
+        datos2.map((d) => {
+            return (
+                <option value={d.id}>{d.estudiante}</option>
+        )})
+      )       
 }
