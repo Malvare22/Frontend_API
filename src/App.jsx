a5e515b05485cfcaf1ee6c6291f0cdfa665ee6b7
Andrés Felipe Sanguino Sanguino <andresfelipesasa@ufps.edu.co>
2023-05-24 21:22:37 -0500

    Lista de planes de negocio
    Se implementó el listar de planes de negocio para el LiderUE

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import ListarPlanesEstudiante from './components/estudiante/Estudiante_ListarPla
 import ListarIdeasEstudiantetest from './components/estudiante/Estudiante_Card_Idea';
 import LiderVistaIdea from './components/lider/Lider_VerIdea';
 import LiderListarIdeas from './components/lider/Listar_ideas';
+import LiderListarPlanes from './components/lider/Listar_planes';
 import Sidebar from './components/estudiante/Estudiante_Navbar'
 import EditarPerfilEstudiante from './components/estudiante/Estudiante_Perfil_Editar';
 import EstudianteEvaluacion from './components/estudiante/Estudiante_Evaluacion';
@@ -56,6 +57,7 @@ const Enrutado = () => {
           </Route>
           <Route element={<TemplateLider></TemplateLider>}>
             <Route path='/Lider/Ideas' element={<LiderListarIdeas></LiderListarIdeas>}></Route>
+            <Route path='/Lider/Planes' element={<LiderListarPlanes></LiderListarPlanes>}></Route>
             <Route path='/Lider/VistaIdea' element={<LiderVistaIdea></LiderVistaIdea>}/>
           </Route>
           <Route element={<TemplateAdministrativo></TemplateAdministrativo>}>
--- src/components/lider/Listar_ideas.jsx
+++ src/components/lider/Listar_ideas.jsx
@@ -62,7 +62,7 @@ const Table = ({ data }) => {
   const navigate = useNavigate();
   const toggleA = () => {
     navigate('/Lider/VistaIdea');
-};
+  };
   return (
     <Sdiv>
       <div className='w-auto'>
@@ -166,6 +166,18 @@ const Filters = ({ onFilter }) => {
       fechaInicio,
       fechaFin
     };
+    if ((fechaInicio && !fechaFin) || (!fechaInicio && fechaFin)) {
+      alert("Se requiere tanto la fecha de inicio como la fecha de fin para filtrar.");
+      return;
+    }
+    if (fechaInicio && fechaFin) {
+      const dateInicio = new Date(fechaInicio);
+      const dateFin = new Date(fechaFin);
+      if (dateInicio > dateFin) {
+        alert("La fecha de inicio no puede ser mayor que la fecha de fin.");
+        return;
+      }
+    }
     onFilter(filters);
   };
 
@@ -243,12 +255,12 @@ export default function Listar_Ideas() {
     try {
       let value = null;
       value = await axios.get('../ideasFiltradas.json').then(
-      response => {
-        const data = response.data;
-        return data;
-      }).catch(error => {
-        console.error(error);
-      });
+        response => {
+          const data = response.data;
+          return data;
+        }).catch(error => {
+          console.error(error);
+        });
       setFilteredData(value);
     } catch (error) {
       console.error(error);
