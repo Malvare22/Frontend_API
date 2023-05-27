a039ed07f4ea343a9034f611c71fca1708ab7dea
JulianQuirozG <Julianquga@ufps.edu.co>
2023-05-27 12:27:38 -0500

    VistaIdea Funcional
    Consime el nuevo json y alterna entre los botones ademas, de que salen los alert de docentes

--- src/components/lider/Lider_Evaluacion.jsx
+++ src/components/lider/Lider_Evaluacion.jsx
@@ -21,6 +21,24 @@ const Evaluaciones = (props) => {
         setViewAlert(!viewAlert);
     }
 
+    const eliminar = (a) => {
+        console.log(a);
+        ObtenerIdElimnar(a);
+        bottomEliminar();
+        
+    }
+
+    const [viewEliminar, setViewEliminar] = useState(false);
+    const bottomEliminar = () => {
+        setViewEliminar(!viewEliminar);
+    }
+
+    const [idEliminar, setidEliminar] = useState(String);
+    const ObtenerIdElimnar = (a) => {
+        setidEliminar(a);
+    }
+
+    //AXIOS PARA RECIBIR LOS DOCENTES
     const [profesores, setProfesores] = useState([]);
     const getProfesores = async () => {
         let value = null;
@@ -37,6 +55,7 @@ const Evaluaciones = (props) => {
         getProfesores();
     }, []);
 
+    //AXIOS PARA RECIBIR A LOS DOCENTES CALIFICADORES CON SUS NOTAS Y OBSERVACIONES
     const [calificadores, setCalificadores] = useState([]);
     const getCalificadores = async () => {
         let value = null;
@@ -77,91 +96,109 @@ const Evaluaciones = (props) => {
                                 <div className="mt-3">
                                     <div className="row">
                                         <div className="col-6">
-                                            <div className="container rounded-1 mb-3" style={{ background: "#B4B4B4" }}>
+                                            <div className="container rounded-1 mb-3 p-4" style={{ background: "#B4B4B4" }}>
                                                 <p className="py-2" style={{ color: "#000" }}><b>Calificación:</b></p>
                                                 <p className="d-flex justify-content-end" style={{ color: "#000", fontSize: "30px" }}><b>{props.estado}</b></p>
                                                 <p className="py-2" style={{ color: "#000" }}><b>Observaciones:</b></p>
-                                                <div className="row">
-                                                    <div className="col-auto">
-                                                        <p style={{ color: "#000" }}>Evaluador 1: </p>
-                                                    </div>
-                                                    <div className="col-auto">
-                                                        <p style={{ color: "#000" }}>{props.observacion}</p>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col-auto">
-                                                        <p style={{ color: "#000" }}>Evaluador 2: </p>
-                                                    </div>
-                                                    <div className="col-auto">
-                                                        <p style={{ color: "#000" }}>{props.observacion}</p>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col-auto">
-                                                        <p style={{ color: "#000" }}>Evaluador 3: </p>
-                                                    </div>
-                                                    <div className="col-auto">
-                                                        <p style={{ color: "#000" }}>{props.observacion}</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-6">
-                                            <div className="container rounded-1" style={{ background: "#B4B4B4" }}>
-                                                <p className="py-2 d-flex justify-content-center" style={{ color: "#000" }}><b>Comité de evaluación</b></p>
-                                                {calificadores.map((v, i) => {
-                                                    if (v.docente) {
-                                                        let colorin="";
-                                                        if(v.calificacion === 'a'){
-                                                            colorin="#2B9877"
-                                                        }else if(v.calificacion === 'r'){
-                                                            colorin="#DC4B4B"
-                                                        }else{
-                                                            colorin="#000000"
-                                                        }
-                                                        console.log(i+" "+v.docente);
+
+                                                {/* {console.log("id",props.identificador)}
+                                                {console.log("calificadores",calificadores)}
+                                                {console.log("profesores",profesores)}
+                                                {console.log()} */}
+
+                                                 { calificadores.length != 0 && calificadores[props.identificador].calificacionesInfo.map((v, i) => {            
                                                     
-                                                    return   <div key={i} className="row">
-                                                            <div className="col-12">
-                                                                <p style={{ color: "#000" }}><b>Evaluador {i+1}: </b></p>
-                                                            </div>
-                                                            <div className="col-8">
-                                                                <p style={{ color: "#000" }}>{v.docente}</p>
-                                                            </div>
-                                                            <div className="col-2">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={colorin} class="bi bi-square-fill" viewBox="0 0 16 16">
-  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z"/>
-</svg>
-                                                            </div>
-                                                            <div className="col-2">
-                                                                <p style={{ color: "#000" }}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
-  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/>
-</svg></p>
+                                                    if (v.observacion != null ) {
+
+                                                        return <div key={i} className="row">
+                                                        <div className="row">
+                                                                <div className="col-auto">
+                                                                    <p style={{ color: "#000" }}>Evaluador {i+1}: </p>
+                                                                </div>
+                                                                <div className="col-auto">
+                                                                    <p style={{ color: "#000" }}>{v.observacion}</p>
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                    
-                                                    }else{
-                                                        docentes=false;
+
+                                                    } else {
+                                                        docentes = false;
                                                         return <div key={i} className="row">
-                                                            <div className="col-12">
-                                                                <p style={{ color: "#000" }}><b>Evaluador {i+1}: </b></p>
+                                                            <div className="row">
+                                                                <div className="col-auto">
+                                                                    <p style={{ color: "#000" }}>Evaluador {i+1}: </p>
+                                                                </div>
+                                                                <div className="col-auto">
+                                                                    <p style={{ color: "#000" }}></p>
+                                                                </div>
                                                             </div>
-                                                            <div className="col-8">
-                                                                <p style={{ color: "#000" }}>No asignado</p>
-                                                            </div>
-                                                            </div>
-                                                        
+                                                        </div>
+
                                                     }
 
                                                 })}
 
 
 
+                                            </div>
+                                        </div>
+                                        <div className="col-6">
+                                            <div className="container rounded-1 p-4" style={{ background: "#B4B4B4" }}>
+                                                <p className="py-2 d-flex justify-content-center" style={{ color: "#000" }}><b>Comité de evaluación</b></p>
+                                                
+                                                 { calificadores.length != 0 && calificadores[props.identificador].calificacionesInfo.map((v, i) => {
+                                                    
+                                                    if (v.id.codigoDocente != null) {
+                                                        let colorin = "";
+                                                        if (v.estado === 'aprobado') {
+                                                            colorin = "#2B9877"
+                                                        } else if (v.estado === 'rechazada') {
+                                                            colorin = "#DC4B4B"
+                                                        } else {
+                                                            colorin = "#000000"
+                                                        }
+                                                        return <div key={i} className="row">
+                                                            <div className="col-12">
+                                                                <p style={{ color: "#000" }}><b>Evaluador {i + 1}: </b></p>
+                                                            </div>
+                                                            <div className="col-8">
+                                                                <p style={{ color: "#000" }}>{v.id.codigoDocente}</p>
+                                                            </div>
+                                                            <div className="col-2">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill={colorin} className="bi bi-square-fill border rounded-2 border-2 border-dark" viewBox="0 0 16 16">
+                                                                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
+                                                                </svg>
+                                                            </div>
+                                                            {props.estado == "NA" ? colorin === "#000000" ? <div className="col-2">
+                                                                <p style={{ color: "#000" }}> <svg style={{ cursor: "pointer" }} xmlns="http://www.w3.org/2000/svg" onClick={() => eliminar(v.id.codigoDocente)} width="24" height="24" fill="currentColor" className="bi bi-x-square-fill" viewBox="0 0 16 16">
+                                                                    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
+                                                                </svg></p>
+                                                            </div> : <div className="col-2">
+                                                                <p style={{ color: "#000" }}> <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#555555" className="bi bi-x-square-fill disabled" viewBox="0 0 16 16">
+                                                                    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
+                                                                </svg></p>
+                                                            </div> : ""
 
-                                                {console.log(props.estado)}
+                                                            }
+                                                        </div>
+
+                                                    } else {
+                                                        docentes = false;
+                                                        return <div key={i} className="row">
+                                                            <div className="col-12">
+                                                                <p style={{ color: "#000" }}><b>Evaluador {i + 1}: </b></p>
+                                                            </div>
+                                                            <div className="col-8">
+                                                                <p style={{ color: "#000" }}>No asignado</p>
+                                                            </div>
+                                                        </div>
+
+                                                    }
+
+                                                })}
+
                                                 {
-                                                    props.estado == "NA" ? docentes ? "" :<div className="row d-flex justify-content-end">
+                                                    props.estado == "NA" ? docentes ? "" : <div className="row d-flex justify-content-end">
                                                         <button className="btn btn-sm" onClick={toggleAlert} style={{ backgroundColor: "transparent", width: "auto", border: "none" }}>
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-add" viewBox="0 0 16 16">
                                                                 <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
@@ -176,7 +213,9 @@ const Evaluaciones = (props) => {
                                                     <p style={{ color: "#000" }}><b>Fecha de corte: </b></p>
                                                 </div>
                                                 <div className="col-auto">
-                                                    <p style={{ color: "#000" }}>{props.fecha}</p>
+                                                    <p style={{ color: "#000" }}>{calificadores != 0 ? 
+                                                    
+                                                    calificadores[props.identificador].fechaCorte[2]+"/"+calificadores[props.identificador].fechaCorte[1]+"/"+calificadores[props.identificador].fechaCorte[0]:""}</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -205,9 +244,9 @@ const Evaluaciones = (props) => {
                         </Input>
                         <Label for="exampleSelectMulti">Select Multiple</Label>
                         <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                            {profesores.map((l) => {
+                            {profesores.map((l,i) => {
                                 if (l.area === Area) {
-                                    return (<option value={l.docente}>{l.docente}</option>);
+                                    return (<option key={l.docente+i} value={l.docente}>{l.docente}</option>);
                                 } else {
                                     return ("");
                                 }
@@ -219,6 +258,19 @@ const Evaluaciones = (props) => {
                 <ModalFooter>
                     <Button color="danger">Asignar</Button>
                     <Button color="primary" onClick={toggleAlert}>Cancelar</Button>
+                </ModalFooter>
+            </Modal>
+
+            <Modal centered isOpen={viewEliminar}>
+                <ModalBody>
+                    <FormGroup>
+                        <Label id="texto">¿Quieres eliminar a este docente tutor {idEliminar}?</Label>
+                    </FormGroup>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button color="danger">Eliminar</Button>
+                    <Button color="primary" onClick={bottomEliminar}>Cancelar</Button>
                 </ModalFooter>
             </Modal>
         </main>
