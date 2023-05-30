9e9c61d9af4ca2757e2eda80bb835ecf62288a29
Andrés Felipe Sanguino Sanguino <andresfelipesasa@ufps.edu.co>
2023-05-30 18:26:06 -0500

    Vista de establecer contraseña con validaciones

--- src/routes/login/ResetPassword.jsx
+++ src/routes/login/ResetPassword.jsx
@@ -6,50 +6,70 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
+
 export default function Screen() {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [confirmationMessage, setConfirmationMessage] = useState('');
     const navigate = useNavigate();
     const backToggler = () => {
         navigate('/login');
     };
-    return (
-        <div className='container-fluid' style={{ background: "#1C3B57" }}>
-            <div className='row p-5 d-flex justify-content-center'>
-                <div className='col-md-6 col-12 p-5 d-flex justify-content-center align-items-center' style={{ background: "#68462C" }}>
-                    <div className='flex-grow-1'>
-                        <h2 className='text-center mb-5' style={{ color: "white", fontWeight: "bold" }}>SoftUE</h2>
-                        <div className='container border rounded m-0' style={{ backgroundColor: "#D9D9D9" }}>
-                            <div className='text-center m-3'>
-                                <img src={logo} className='' style={{ width: "30%" }}/>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password === '') {
+            console.log('Ingresa una contraseña.');
+        }
+        else if (password === confirmPassword) {
+            setConfirmationMessage('¡Contraseña cambiada exitosamente!');
+        } else {
+            console.log('Las contraseñas no coinciden, por favor verifique.');
+        }
+    };
+    return (<div className="container-fluid" style={{ background: "#1C3B57" }}>
+        <div className="row p-5 d-flex justify-content-center">
+            <div className="col-md-6 col-12 p-5 d-flex justify-content-center align-items-center" style={{ background: "#68462C" }}>
+                <div className="flex-grow-1">
+                    <h2 className="text-center mb-5" style={{ color: "white", fontWeight: "bold" }}>SoftUE</h2>
+                    <div className="container border rounded m-0" style={{ backgroundColor: "#D9D9D9" }}>
+                        <div className="text-center m-3">
+                            <img src={logo} className="" style={{ width: "30%" }} alt="Logo" />
+                        </div>
+                        {confirmationMessage ? (
+                            <div className="text-center mt-4">
+                                <p>{confirmationMessage}</p>
                             </div>
+                        ) : (
                             <StyledDiv>
-                                <form>
+                                <form onSubmit={handleSubmit}>
                                     <div>
-                                        <p className='text-center mt-4 fw-bold'>Introduzca la nueva contraseña</p>
+                                        <p className="text-center mt-4 fw-bold">Introduzca la nueva contraseña</p>
 
-                                        <div className='d-flex justify-content-center'>
-                                            <input className='form-control border-0 border-bottom rounded-0 border-dark shadow-none w-75' style={{ backgroundColor: "#D9D9D9" }}></input>
+                                        <div className="d-flex justify-content-center">
+                                            <input className="form-control border-0 border-bottom rounded-0 border-dark shadow-none w-75" style={{ backgroundColor: "#D9D9D9" }} type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                                         </div>
-                                        <p className='text-center mt-4 fw-bold'>Repita la nueva contraseña</p>
+                                        <p className="text-center mt-4 fw-bold">Repita la nueva contraseña</p>
 
-                                        <div className='d-flex justify-content-center'>
-                                            <input className='form-control border-0 border-bottom rounded-0 border-dark shadow-none w-75' style={{ backgroundColor: "#D9D9D9" }}></input>
+                                        <div className="d-flex justify-content-center">
+                                            <input className="form-control border-0 border-bottom rounded-0 border-dark shadow-none w-75" style={{ backgroundColor: "#D9D9D9" }} type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
                                         </div>
                                     </div>
-                                    <div className='d-flex justify-content-center align-content-center'>
-                                        <button type="button" className="btn m-4" style={{ backgroundColor: "#2B9877", color: "white" }}>
-                                            <p className='d-flex align-items-center justify-content-center m-1'>Volver</p>
+                                    <div className="d-flex justify-content-center align-content-center">
+                                        <button type="button" className="btn m-4" style={{ backgroundColor: "#2B9877", color: "white" }} onClick={backToggler}>
+                                            <p className="d-flex align-items-center justify-content-center m-1">Volver</p>
                                         </button>
                                         <button type="submit" className="btn m-4" style={{ backgroundColor: "#2B9877", color: "white" }}>
-                                            <p className='d-flex align-items-center justify-content-center m-1'>Cambiar contraseña</p>
+                                            <p className="d-flex align-items-center justify-content-center m-1">Cambiar contraseña</p>
                                         </button>
                                     </div>
                                 </form>
                             </StyledDiv>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     );
 }
 
@@ -58,9 +78,3 @@ const StyledDiv = styled.div`
         p{font-size: 1rem;}
     }
 `;
-
-
-
-
-
-
