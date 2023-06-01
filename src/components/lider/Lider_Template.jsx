5639aaad9abccdd9c3b72bb9ea5b4a104de496d1
Rodrigo Andrés Malaver Suárez <72281004+Malvare22@users.noreply.github.com>
2023-05-31 22:33:21 -0500

    Pingo (#59)
    * Agregadas dos lineas
    
    * Agregado componente tabla
    
    * Terminadas partes visuales de perfil estudiante (edicion y perfil)
    
    * Hechas validaciones de campos en la edición de perfil de Estudiante
    
    * Correción tabla
    
    * Iniciando reestructuracion de la plantilla de estudiante
    
    * Corregida columna izquierda de plantilla estudiante
    
    * Solucionado problemas de toggle button en NavBarEstudiante
    
    * Navbar de Estudiante Responsivo y arreglado finalmente
    
    * Navbars arregladas
    
    * Corregido el merge
    
    * Finalizado prototipo base de editar perfil de usuario-estudiante
    
    Resta la edicion del perfil con el backend y actualizar el useContext
    
    * Perfil y edicion propio de estudiante cargan de una const .json
    
    * Implementacion de loader al ver y editar estudiante
    
    Las paginas editar y ver perfil de estudiante cuentan con una
    verificacion previa a la carga, los datos se comparten mediante local
    storage
    
    * Verificaciones establecias para ver y editar perfil estudiante por parte del lider
    
    * Agregada funcionalidad de previsualizacion de imagen
    
    * Trabajando en registro estudiante
    
    * En teoria resta probar el envio de imagen
    
    En espera de especificacion del codigo de los estudiantes
    
    * Eliminado rastros de atributo codigo en registrar estudiante
    
    * Agregado campo de contrasenia al registrar estudiante
    
    * Agregado al input file los archivos ideales
    
    * Perfeccionamiento de editar estudiante por parte del lider
    
    * Arreglados ver y editar estudiante por parte del lider
    
    * Correccion de edicion y ver perfil de estudiante
    
    * Agregados perfil y vista por parte del lider de UE de admin y docente
    
    * Agregadas vistas y edición de todos los roles menos administrativo
    
    * Reparada edición de perfil de docente
    
    * Estudiantes Works?
    
    * Esperando Tabla de Docente y un .json
    
    * Iniciada integración con el backend
    
    * Ya es posible registrar usuarios :D
    
    * Deshabilitar alumno
    
    * Mostrar perfil
    
    * Se muestran parcialmente los datos de editar perfil
    
    * Listar Docentes
    
    * Reparado mostrar imagen
    
    * Imagen by default para docente
    
    * Registrar docente
    
    Falta setear una imagen por defecto y validar excepciones
    
    * Iniciar sesion con localstorage

--- src/components/lider/Lider_Template.jsx
+++ src/components/lider/Lider_Template.jsx
@@ -1,26 +1,39 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, json, useNavigate } from "react-router-dom";
 import NavbarLU from '../lider/Lider_Navbar'
 import styled from "styled-components";
+import Login from "../../routes/login/Login";
+import { useEffect } from "react";
+import Error404 from "../../routes/404_error";
+import Footer from "../Footer";
 
 export default function TemplateLider({ children }) {
     /*Método de validación: aplica para todo usuario, es preguntar si el usuario corresponde al rol*/
-
-    return (<><div className="container-fluid row m-0 p-0">
-        <div className="col-12 col-sm-2 m-0 p-0">
-            <NavbarLU></NavbarLU>
-        </div>
-        <div className="col-12 col-sm-10 m-0 p-0 d-flex align-items-center justify-content-center">
-            <Contenido>
-                <Outlet></Outlet>
-            </Contenido>
-        </div>
-    </div></>);
+    const navigate = useNavigate()
+    const Token = localStorage.getItem('token_access')
+    const Usuario = JSON.parse(localStorage.getItem('session'))
+    if(JSON.parse(localStorage.getItem('session')).rol == 'coordinador')
+    return (
+        <>
+            <div className="container-fluid row m-0 p-0">
+                <div className="col-12 col-sm-2 m-0 p-0">
+                    <NavbarLU></NavbarLU>
+                </div>
+                <div className="col-12 col-sm-10 m-0 p-0 d-flex align-items-center justify-content-center">
+                    <Contenido>
+                        <Outlet></Outlet>
+                    </Contenido>
+                </div>
+            </div>
+            <Footer></Footer>
+        </>
+    );
+    else return(<Error404></Error404>)
 
 }
 
 const Contenido = styled.div.attrs({
     className: 'flex-grow-1',
-    
+
 })`
     min-height:100vh;
 `;
