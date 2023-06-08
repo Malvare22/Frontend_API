feae54a3eaf3016f4d76670f3a2248090396a355
María Velásquez <alexandra19vela@gmail.com>
2023-06-07 22:01:46 -0500

    Tabla de listar preguntas sin funcionalidades

--- src/components/lider/Lider_Preguntas_Listar.jsx
+++ src/components/lider/Lider_Preguntas_Listar.jsx
@@ -1,12 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { obtenerTabla } from '../useGeneral/Tabla';
 
 export default function listarPreguntas() {
+    let datos = [
+        {
+            "id" : 1,
+            "Enunciado" : "Triste estoy",
+            "Componente" : "Liderazgo",
+        },
+        {
+            "id" : 1,
+            "Enunciado" : "feliz estoy",
+            "Componente" : "Liderazgo",
+        },
+        {
+            "id" : 1,
+            "Enunciado" : "enojada estoy",
+            "Componente" : "Liderazgo",
+        },
+        {
+            "id" : 1,
+            "Enunciado" : "Fastidiada estoy",
+            "Componente" : "Liderazgo",
+        },
+        {
+            "id" : 1,
+            "Enunciado" : "Fastidiada estoy",
+            "Componente" : "Liderazgo",
+        },
+        {
+            "id" : 1,
+            "Enunciado" : "Fastidiada estoy",
+            "Componente" : "Liderazgo",
+        }
+    ];
+    let columnas = ["id", "Enunciado", "Componente"];
+        
+    const handleEliminarClick = (dato) => {
+        console.log(dato);
+    }
+    
+    const handleEditarClick = (dato) => {
+        console.log(dato);
+    }
+
     return (
-        <div>
-            <h1>Hola mundo</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil culpa velit accusamus repellat inventore similique minima, aliquid vitae iure cum ut odit? Rerum ad vitae architecto neque iusto minima quis?</p>
+        <div className='m-5'>
+            <h1 className="fst-italic fw-bold fs-1 text-black mb-4">Preguntas Evaluacion de emprendimiento</h1>
+            {obtenerTabla({ datos, columnas, handleEliminarClick, handleEditarClick})}
         </div>
     );
 }
