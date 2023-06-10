23ab20ebc5d7beb79f1cd7ca656009a56f236819
Julian <josejulianalre@ufps.edu.co>
2023-06-10 11:31:36 -0500

    Inicio de evaluacion de competencias estudiante

--- src/components/estudiante/Estudiante_Preguntas.jsx
+++ src/components/estudiante/Estudiante_Preguntas.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
+
+
+
 const EstudiantePreguntas = (props) => {
     return (
-        <div>
-            {props.coso}
+        <div className='p-3 pt-2'>
+            <div style={{wordWrap : "breack-word"}}>
+                <p className='fw-bold '>
+                    {props.enunciado}
+                </p>
+            </div>
+
+            {props.respuestas.map((v, i) =>{
+                return(
+                <div className='row d-flex'>
+                    <div className="col-auto">
+                        <input required className='mx-1' type="radio"  value="" name=''/>
+                    </div>
+                    <div className="col-auto">
+                        <p className='m-0' >{v.contenido}</p>
+                    </div>
+                </div>
+                )
+            })}
+            
         </div>
     );
 };
