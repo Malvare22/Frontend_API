49b5562b4987768eff7a1814f9cac02ed28993fa
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-06-13 04:52:38 -0500

    update

--- src/components/docente/Docente_Tutor_Idea_Ver.jsx
+++ src/components/docente/Docente_Tutor_Idea_Ver.jsx
@@ -200,7 +200,7 @@ const InfoGeneral = (props) => {
 
         try {
             //let URLd = 'http://144.22.37.238:8080/ideaNegocio/integrantes/'+props.nombre+'/'+estudiante;
-            let URLd = 'http://144.22.32.132:8080/ideaNegocio/integrantes/' + props.nombre + '/' + estudiante;
+            let URLd = 'http://144.22.32.132:8080/ideaPlanteada' + props.nombre + '/' + estudiante;
             await axios.delete(URLd, {
                 headers: { "X-Softue-JWT": localStorage.getItem("token_access") }
             });
@@ -214,7 +214,7 @@ const InfoGeneral = (props) => {
         toggleAlertEstudiante();
         try {
             //let URLd = 'http://144.22.37.238:8080/ideaNegocio/integrantes/sie/'+estudiante;
-            let URLd = 'http://144.22.32.132:8080/ideaNegocio/integrantes/' + props.nombre + '/' + estudiante;
+            let URLd = 'http://144.22.32.132:8080/ideaPlanteada' + props.nombre + '/' + estudiante;
             await axios.post(URLd, null, {
                 headers: { "X-Softue-JWT": localStorage.getItem("token_access") }
             });
