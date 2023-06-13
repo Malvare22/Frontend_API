b08cfee973edbc9fd433d418469ba1afa8246ddc
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2023-06-13 18:10:01 -0500

    Fix edicion y registro de usuarios por el lider

--- src/components/useGeneral/Listar_Estudiantes.jsx
+++ src/components/useGeneral/Listar_Estudiantes.jsx
@@ -122,7 +122,7 @@ const Table = (props) => {
                                                 <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
                                             </svg>
                                         </button>
-                                        {props.type!='docente' && <button type="button" className="btn" onClick={()=>toggleB(d)} value={d.id} style={{ width: "auto", border: "none" }}>
+                                        {(props.type!='docente' && props.type!='lider') && <button type="button" className="btn" onClick={()=>toggleB(d)} value={d.id} style={{ width: "auto", border: "none" }}>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
@@ -209,7 +209,7 @@ export default function ListarEstudiantes(props) {
                         <br></br>
                         <Table data={filteredData} type={props.type} updater={getEstudiantes}></Table>
                         <br></br>
-                        {props.type!='docente' && <div className="d-flex justify-content-between">
+                        {(props.type!='docente' && props.type!='lider')&& <div className="d-flex justify-content-between">
                             <button type="button" className="btn rounded-3" style={{ background: "#1C3B57", color: "#FFFFFF" }} onClick={addStudent}>
                                 <div className="col-auto">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-add" viewBox="0 0 16 16">
