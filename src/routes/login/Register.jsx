f397ed3a36d386b0d442e7c226315d276f90110a
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2023-06-14 16:55:30 -0500

    Register style fix

--- src/routes/login/Register.jsx
+++ src/routes/login/Register.jsx
@@ -10,11 +10,6 @@ import axios from 'axios';
 
 export default function Screen() {
 
-    const navigate = useNavigate();
-    const backToggler = () => {
-        navigate('/login');
-    };
-    const [type, setType] = useState(0);
     return (
         <div className='container-fluid' style={{ background: "#1C3B57" }}>
             <div className='row p-5 d-flex justify-content-center'>
@@ -25,7 +20,7 @@ export default function Screen() {
                             <div className='text-center m-3'>
                                 <img src={logo} className='' style={{ width: "30%" }}></img>
                             </div>
-                            <PanelPrincipal navigate={navigate} backToggler={backToggler}></PanelPrincipal>
+                            <PanelPrincipal></PanelPrincipal>
                         </div>
                     </div>
                 </div>
@@ -35,92 +30,102 @@ export default function Screen() {
 }
 
 const PanelPrincipal = (props) => {
+    const [rol, setRol] = useState(0);
+    const roles = ['Estudiante', 'Docente']
+    const [form, setForm] = useState({ usuario: '', contrasenia: '' });
+
+
+    const initialErrors = {
+        usuario: false,
+        contrasenia: false,
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
+    const [errors, setErrors] = useState(initialErrors);
     const navigate = useNavigate();
-    const handleSubmit = () => {        
+
+    const validar = () => {
+        let newErrors = {
+            usuario: false,
+            contrasenia: false,
+        };
+        let fail = false;
+
+        if (rol == 'Estudiante' || rol=='Docente') {
+            if (isNaN(form.usuario) || form.usuario <= 0) {
+                newErrors.usuario = true;
+                fail = true;
+                alert('El campo de código no puede ser negativo.', 'error');
+            }
+
+            if (form.contrasenia.trim() === '') {
+                newErrors.contrasenia = true;
+                fail = true;
+                alert('La contraseña no puede estar vacía.', 'error');
+            } 
+            
+            else if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&+]).{6,}$/.test(form.contrasenia)) {
+                newErrors.contrasenia = true;
+                fail = true;
+                alert('La contraseña debe contener mayúsculas, minúsculas, números, un carácter especial y tener al menos 6 caracteres.', 'error');
+            }
+        }
+        else{
+            alert('Debe seleccionar un rol.', 'error');
+            fail = true;
+        }
+
+        setErrors(newErrors);
+        return !fail;
+    };
+
+
+    const handleSubmit = () => {
         if (!validar()) {
             return;
         }
-        const { codigo, contrasenia } = formValues;
+        const { usuario, contrasenia } = form;
         var formData = new FormData();
-        formData.append('codigo', codigo);
+        formData.append('codigo', usuario);
         formData.append('contrasenia', contrasenia);
         const config = {
             headers: {
                 "X-Softue-JWT": localStorage.getItem('token_access')
             }
         }
-        axios.post('http://localhost:8080/register/estudiante/codigo', formData, config)
+        if(rol == 'Estudiante'){
+            axios.post('http://localhost:8080/register/estudiante/codigo', formData, config)
             .then((response) => {
                 alert("Se ha creado exitosamente su usuario, ya puede iniciar sesión con su código institucional y contraseña.")
-                navigate('/Login');
+                props.navigate('/Login');
             })
             .catch((error) => {
                 alert(error.response.data.errorMessage);
             });
-    };
-    const initialErrors = {
-        codigo: false,
-        contrasenia: false,
-    };
-    const [formValues, setFormValues] = useState({ codigo: '', contrasenia: '' });
-    const [errors, setErrors] = useState(initialErrors);
-    const validar = () => {
-        let newErrors = {
-            codigo: false,
-            contrasenia: false,
-        };
-        let fail = false;
-        if (formValues.contrasenia.trim() === '') {
-            newErrors.contrasenia = true;
-            fail = true;
-            alert('La contraseña no puede estar vacía.', 'error');
-        } else if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&+]).{6,}$/.test(formValues.contrasenia)) {
-            newErrors.contrasenia = true;
-            fail = true;
-            alert('La contraseña debe contener mayúsculas, minúsculas, números, un carácter especial y tener al menos 6 caracteres.', 'error');
         }
-        if (isNaN(formValues.codigo) || formValues.codigo <= 0) {
-            newErrors.codigo = true;
-            fail = true;
-            alert('El campo de código no puede ser negativo.', 'error');
-        }
-        setErrors(newErrors);
-        return !fail;
     };
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value });
-    };
-    const [type, setType] = useState('password');
-    const changeType = () => {
-        setType(type === 'password' ? 'text' : 'password');
-    };
+
+    const toggleSelect = (e) => {
+        const { value } = e.target
+        setRol(value)
+    }
+
     return (
         <StyledDiv>
             <form>
-                <div>
-                    <p className='text-center mt-4 fw-bold'>Introduzca su código institucional de estudiante:</p>
-                    <div className='d-flex justify-content-center'>
-                        <input type='number' min={0} onChange={handleChange} className={`required form-control border-0 border-bottom rounded-0 border-dark shadow-none w-75 ${errors.codigo ? 'is-invalid' : ''}`} name='codigo' value={formValues.codigo} style={{ backgroundColor: "#D9D9D9" }}/>
-                    </div>
-                </div>
-                <div>
-                    <p className='text-center mt-4 fw-bold'>Establezca una contraseña:</p>
-                    <div className='d-flex justify-content-center'>
-                        <input onChange={handleChange} type={type} className={`required form-control border-0 border-bottom rounded-0 border-dark shadow-none w-75 ${errors.contrasenia ? 'is-invalid' : ''}`} name='contrasenia' value={formValues.contrasenia} style={{ backgroundColor: "#D9D9D9" }} />
-                        {type === 'password' ? (
-                            <svg xmlns="http://www.w3.org/2000/svg" onClick={changeType} className='bi bi-eye-fill' width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-                            </svg>
-                        ) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" onClick={changeType} width="20" height="20" fill="currentColor" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
-                                <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
-                                <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
-                            </svg>
+                <div className='d-flex justify-content-center'>
+                    <select className="form-select w-75" onChange={(e) => toggleSelect(e)} aria-label="Default select example">
+                        <option selected value={0}>Seleccione el rol de su usuario</option>
+                        {roles.map((r) =>
+                            <option value={r}> {r}</option>
                         )}
-                    </div>
+                    </select>
                 </div>
+                {rol != 0 && <RegisterForm rol={rol} form={form} handleChange={handleChange} errors={errors}/>}
                 <div className='d-flex justify-content-center align-content-center'>
                     <button type="button" className="btn m-4" style={{ backgroundColor: "#2B9877", color: "white" }}>
                         <p onClick={() => navigate('/login')} className='d-flex align-items-center justify-content-center m-1'>Volver</p>
@@ -131,6 +136,42 @@ const PanelPrincipal = (props) => {
         </StyledDiv>
     );
 };
+
+const RegisterForm = (props) => {
+
+    const [type, setType] = useState('password');
+
+    const changeType = () => {
+        setType(type === 'password' ? 'text' : 'password');
+    };
+
+    return (<>
+        <div>
+            <p className='text-center mt-4 fw-bold'>{"Introduzca su código institucional de estudiante:"}</p>
+            <div className='d-flex justify-content-center'>
+                <input type='number' min={0} onChange={props.handleChange} className={`required form-control border-0 border-bottom rounded-0 border-dark shadow-none w-75 ${props.errors.usuario ? 'is-invalid' : ''}`} name='usuario' value={props.form.usuario} style={{ backgroundColor: "#D9D9D9" }} />
+            </div>
+        </div>
+        <div>
+            <p className='text-center mt-4 fw-bold'>Establezca una contraseña:</p>
+            <div className='d-flex justify-content-center'>
+                <input onChange={props.handleChange} type={type} className={`required form-control border-0 border-bottom rounded-0 border-dark shadow-none w-75 ${props.errors.contrasenia ? 'is-invalid' : ''}`} name='contrasenia' value={props.form.contrasenia} style={{ backgroundColor: "#D9D9D9" }} />
+                {type === 'password' ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" onClick={changeType} className='bi bi-eye-fill' width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                        <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                    </svg>
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" onClick={changeType} width="20" height="20" fill="currentColor" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+                        <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
+                        <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
+                    </svg>
+                )}
+            </div>
+        </div>
+    </>)
+}
+
 const StyledDiv = styled.div`
     @media only screen and  (max-width: 768px){
         p{font-size: 1rem;}
